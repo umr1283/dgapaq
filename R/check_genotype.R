cbdacdf4033fd8d7a897b4d665149efaeb4fa305
NING-L <lijiao.ning91@gmail.com>
2020-12-23 14:29:44 +0100

    use explicit return; update args description

--- R/check_genotype.R
+++ R/check_genotype.R
@@ -1,29 +1,35 @@
 #' create_genotype_matrix
 #'
-#' Creates a raw genotype matrix with provided VCF files.
+#' Create a raw genotype matrix using VCFs.
 #'
-#' @param sample_sheet A `data.frame`. A data frame with two mandatory columns `id` and `vcf_file`.
-#' @param output_name A `character`. The prefix for output genotype data. Default is "raw_merged"
+#' @param sample_sheet A `data.frame`. The data frame should contain two columns: sample `id` and
+#'     the path to VCFs `vcf_file`.
+#' @param output_name A `character`. The prefix of output file. Default is `raw_merged`.
 #' @param output_directory A `character`. The path to the output directory. Default is `NULL`.
-#' @param chromosomes A vector of `character`. Chromosomes to keep
-#'     (same nomenclature as chromosome name in VCF).
-#'     If argument not specified (as default `NULL`), this filter will be turned off.
+#' @param chromosomes A `character` vector. The chromosomes to keep (same nomenclature as chromosome
+#'     in VCF).
+#'     If argument not specified (default is `NULL`), this filter will be turned off.
 #' @param remove_multiallelic A `logical`. Does the multi-allelic variants (i.e.: genotype composed
-#'     of different alternative alleles) should be removed. Default is `TRUE`.
-#' @param split_multiallelic A `logical`. Does the multi-allelic variants should be splitted into
-#'     bi-allelic variants. Default is `FALSE`.
-#' @param split_type A `character`. The type of variants should be splitted by `bcftools norm`.
+#'     of different alternative alleles) should be removed.
+#'     Default is `TRUE`.
+#' @param split_multiallelic A `logical`. Does the multi-allelic variants should be split into
+#'     bi-allelic variants.
+#'     Default is `FALSE`.
+#' @param split_type A `character`. The type of variants which should be split using `bcftools norm`
+#'     (http://samtools.github.io/bcftools/bcftools.html#norm), value can be one of "snps", "indels",
+#'     "both" or "any".
 #'     Default is `both`.
-#' @param ref_fasta A `character`. A path to the reference fasta file, it should be provided
+#' @param ref_fasta A `character`. The path to the reference fasta file, it should be provided
 #'     if multiallelic INDELs need left-alignment after splitting.
 #'     Default is `NULL` which lead to no left-alignment.
 #' @param bin_path A `list(character)`. A list giving the binary path of `vcftools`, `bcftools`,
 #'     `tabix` and `bgzip`.
-#' @param nb_cores An `integer`. The number of CPUs to use. Default is `1`.
+#' @param nb_cores A `numeric`. The number of CPUs to use. Default is `1`.
 #'
 #' @return A genotype matrix coded NA, 1, or 2 (additive model), with variants in rows and
 #'     individuals in columns. The first column `var_id` is composed of variant's chromosome,
 #'     position, reference allele and alternative allele, separated by "_".
+#'     Half-called genotype is treated as missing.
 #'
 #' @importFrom parallel mclapply
 #' @importFrom data.table `:=`
@@ -47,7 +53,7 @@ create_genotype_matrix <- function(
     bcftools = "/usr/bin/bcftools",
     tabix = "/usr/bin/tabix"
   ),
-  nb_cores = 1L
+  nb_cores = 1
 ) {
 
   if (! all(c("id", "vcf_file") %in% colnames(sample_sheet))) {
@@ -63,7 +69,7 @@ create_genotype_matrix <- function(
   }
 
   if (split_multiallelic & ! split_type %in% c("both", "snps", "indels", "any")) {
-    stop("'split_type' should be either 'both', 'snps' or 'indels'.")
+    stop("'split_type' should be either 'both', 'any', 'snps' or 'indels'.")
   }
 
   `#CHROM` = "ALT" = "CHROM" = "FORMAT" = "POS" = "REF" = "chr" = "idx" = "var_id" <- NULL
@@ -76,10 +82,12 @@ create_genotype_matrix <- function(
     sample_sheet <- sample_sheet[check_exist, ]
   }
 
-  invisible(
-    parallel::mclapply(X = sample_sheet$id, mc.cores = nb_cores, function(iid){
-      ivcf <- sample_sheet$vcf_file[sample_sheet$id %in% iid]
-      iout <- file.path(output_tmp_dir, paste0(iid, "_tmp.vcf.gz"))
+  invisible(parallel::mclapply(
+    X = seq_along(sample_sheet[["id"]]),
+    mc.cores = min(nb_cores, nrow(sample_sheet)),
+    FUN = function(i) {
+      ivcf <- sample_sheet[["vcf_file"]][i]
+      iout <- file.path(output_tmp_dir, paste0(sample_sheet[["id"]][i], "_tmp.vcf.gz"))
       is_gzvcf <- grepl("vcf\\.gz$", ivcf)
 
       if (remove_multiallelic) {
@@ -136,37 +144,37 @@ create_genotype_matrix <- function(
   }
   geno_mat <- geno_mat[, var_id := paste(`#CHROM`, POS, REF, ALT, sep = "_")]
   geno_mat <- geno_mat[, .SD, .SDcols = !`#CHROM`:FORMAT]
-  patern_geno_sep <- ifelse(grepl(".{1}/.{1}:", geno_mat[[1]][1]), "/", "|")
+  geno_sep <- ifelse(grepl(".{1}/.{1}:", geno_mat[[1]][1]), "/", "|")
   geno_mat <- geno_mat[
     ,
-    (sample_sheet$id) := lapply(
+    (sample_sheet[["id"]]) := lapply(
       .SD,
       FUN = gsub,
-      pattern = paste0("(.{1}\\", patern_geno_sep, ".{1}):.*"),
+      pattern = paste0("(.{1}\\", geno_sep, ".{1}):.*"),
       replacement = "\\1"),
-    .SDcols = sample_sheet$id
+    .SDcols = sample_sheet[["id"]]
   ]
 
   ## possible genotypes
-  possible_genotypes_chr <- apply(
+  possible_geno <- apply(
     X = expand.grid(c(".", "0", "1"), c(".", "0", "1")),
     MARGIN = 1,
     FUN = paste,
-    collapse = patern_geno_sep
+    collapse = geno_sep
   )
-  possible_genotypes_num <- rep(1, length(possible_genotypes_chr))
-  names(possible_genotypes_num) <- possible_genotypes_chr
-  possible_genotypes_num[grepl(paste0("0\\", patern_geno_sep, "0"), names(possible_genotypes_num))] <- 0
-  possible_genotypes_num[grepl(paste0("1\\", patern_geno_sep, "1"), names(possible_genotypes_num))] <- 2
-  possible_genotypes_num[grepl("\\.", names(possible_genotypes_num))] <- NA
-  possible_genotypes_num <- c(possible_genotypes_num, "0" = 0, "1" = 1, "2" = 2)
+  possible_geno_num <- rep(1, length(possible_geno))
+  names(possible_geno_num) <- possible_geno
+  possible_geno_num[grepl(paste0("0\\", geno_sep, "0"), names(possible_geno_num))] <- 0
+  possible_geno_num[grepl(paste0("1\\", geno_sep, "1"), names(possible_geno_num))] <- 2
+  possible_geno_num[grepl("\\.", names(possible_geno_num))] <- NA
+  possible_geno_num <- c(possible_geno_num, "0" = 0, "1" = 1, "2" = 2)
 
   ## recode genotype
   geno_mat <- geno_mat[
     ,
-    (sample_sheet$id) := lapply(.SD, FUN = function(x) possible_genotypes_num[x]),
-    .SDcols = sample_sheet$id
-  ][, .SD, .SDcols = c("var_id", sample_sheet$id)]
+    (sample_sheet[["id"]]) := lapply(.SD, FUN = function(x) possible_geno_num[x]),
+    .SDcols = sample_sheet[["id"]]
+  ][, .SD, .SDcols = c("var_id", sample_sheet[["id"]])]
 
   data.table::fwrite(
     geno_mat,
@@ -181,29 +189,31 @@ create_genotype_matrix <- function(
 
 #' check_genotype
 #'
-#' Checks the missing genotype against coverage information and impute missing genotype to 0 if
+#' Check missing genotype against coverage information and impute missing genotype to 0 if
 #' coverage is sufficient, otherwise missing genotype is recoded to -1.
 #'
 #'
-#' @param sample_sheet A `data.frame`. A data frame with one mandatory column `id`.
-#'     The column `vcf_file` (path to vcf files) should be provided if you need to create the raw
+#' @param sample_sheet A `data.frame`. The data frame should contain at least the sample `id` column.
+#'     The column `vcf_file` (path to VCFs) should be provided if you need to create the raw
 #'     genotype matrix. Please see details in `create_genotype_matrix()`.
 #'     The column `cov_file` (path to coverage files) should be provided if you need to compress the
-#'     coverage file (output of `samtools depth`) into contiguous segments.
-#'     Please see details in `compress_coverage()`.
-#' @param genotype_matrix A `character` or `NULL`. The path to the genotype matrix to be imported,
-#'     with samples in columns and variants in rows, the first columns should be `var_id` which is
-#'     unique for each variant. `NULL` leading the creation of raw genotype matrix with `create_genotype_matrix()`.
-#' @param min_depth An `integer`. The minimum depth to filter when compressing coverage file.
-#'     Default is `8`.
-#' @param ... Optional arguments to `create_genotype_matrix()`
-#' @param chromosomes A vector of `character`. Chromosomes to check (same nomenclature as chromosome
-#'     name in VCF). If argument not specified (default is `NULL`), this filter will be turned off.
+#'     coverage file (output of `samtools depth`) into contiguous segments. Please see details in
+#'     `compress_coverage()`.
+#' @param genotype_matrix A `character`. The path to the raw genotype matrix to be imported, data
+#'     should have samples in columns and variants in rows, the first column should be `var_id`
+#'     which is the unique ID for each variant.
+#'     Default is `NULL` leading the creation of raw genotype matrix with `create_genotype_matrix()`.
+#' @param min_depth A `numeric`. The minimum depth to keep when compressing coverage files.
+#'     Default is `8`, please see details in `compress_coverage()`.
+#' @param chromosomes A `character` vector. The chromosomes to keep (same nomenclature as chromosome
+#'     in VCF and coverage files).
+#'     If argument not specified (default is `NULL`), this filter will be turned off.
 #' @param output_directory A `character`. The path to the output directory. Default is `NULL`.
 #' @param compress_cov_directory A `character`. The path the to directory of compressed coverage files.
 #'     Default is `NULL`.
-#' @param output_name A `character`. The prefix for output genotype data. Default is "clean_merged".
-#' @param nb_cores An `integer`. The number of CPUs to use. Default is `1`.
+#' @param output_name A `character`. The prefix for output file. Default is `clean_merged`.
+#' @param nb_cores A `numeric`. The number of CPUs to use. Default is `1`.
+#' @param ... Optional arguments to `create_genotype_matrix()`
 #'
 #' @return A genotype matrix coded 0, 1, or 2 (additive model), with variants in rows and individuals
 #'     in columns. Missing genotype is coded to -1. The first column `var_id` is composed of variant's
@@ -224,12 +234,12 @@ check_genotype <- function(
   sample_sheet,
   genotype_matrix = NULL,
   min_depth = 8,
-  ...,
   chromosomes = NULL,
   output_directory = NULL,
   compress_cov_directory = NULL,
   output_name = "clean_merged",
-  nb_cores = 1L
+  nb_cores = 1,
+  ...
 ) {
 
   if (! "id" %in% colnames(sample_sheet)) {
@@ -262,12 +272,11 @@ check_genotype <- function(
     if (any(not_compressed)) {
       cat(paste(
         "Start compressing the coverage file for sample:",
-        paste0(sample_sheet$id[not_compressed], collapse = ", ")
+        paste0(samples_ids[not_compressed], collapse = ", ")
       ), file = check_log, append = TRUE, sep = "\n")
 
-      to_compress <- sample_sheet[not_compressed, ]
       compress_coverage(
-        sample_sheet = to_compress,
+        sample_sheet = sample_sheet[not_compressed, ],
         output_directory = compress_cov_directory,
         min_depth = min_depth,
         chromosomes = chromosomes,
@@ -279,7 +288,7 @@ check_genotype <- function(
     cat("Getting samples' genotype matrix...", file = check_log, append = TRUE, sep = "\n")
     if (is.null(genotype_matrix)) {
       cat(paste0(
-        'Info: raw genotype matrix will be created using files in sample_sheet$vcf_file'
+        'Info: raw genotype matrix will be created using VCFs in sample_sheet$vcf_file'
       ), file = check_log, append = TRUE, sep = "\n")
 
       genotype_matrix <- create_genotype_matrix(
@@ -287,8 +296,8 @@ check_genotype <- function(
         output_name = "raw_merged",
         output_directory = output_directory,
         chromosomes = chromosomes,
-        ...,
-        nb_cores = nb_cores
+        nb_cores = nb_cores,
+        ...
       )
     } else {
       genotype_matrix <- data.table::fread(genotype_matrix, header = TRUE, showProgress = FALSE)
@@ -304,7 +313,7 @@ check_genotype <- function(
     samples_available <- samples_ids %in% colnames(genotype_matrix)
     if (!all(samples_available)) {
       cat(paste(
-        "Following samples do not exist in genotype matrix and will not be check against the coverage info:",
+        "Following samples do not exist in genotype matrix and will not be checked:",
         paste(samples_ids[!samples_available], collapse = ", ")
       ), file = check_log, append = TRUE, sep = "\n")
     }
@@ -344,9 +353,11 @@ check_genotype <- function(
             data.table::inrange(POS, tmp_cov_chr$start, tmp_cov_chr$end)
           ][["idx"]]
           tmp_ind_chr[real_0, (mc_iid) := 0]
-          tmp_ind_chr[is.na(get(mc_iid)), (mc_iid) := -1][, !c("idx", "POS")]
+          tmp_ind_chr[is.na(get(mc_iid)), (mc_iid) := -1]
+          return(tmp_ind_chr[, 2])
         }
       ))
+      return(res_j)
     })
     final_matrix <- cbind(genotype_matrix[, "var_id"], do.call("cbind", res_i))
   })
--- R/compress_coverage.R
+++ R/compress_coverage.R
@@ -1,22 +1,20 @@
 #' compress_coverage
 #'
-#' Compresses coverage file (output of `samtools depth`) into contiguous segments based on position.
-#' This function works with whole genome or any specified chromosomes.
-#' Each sample is supposed to have one coverage file.
-#' The coverage file should not have header and contain 3 mandatory columns for chromosome, position
-#'     and depth, with no header.
+#' Compress coverage file (output of `samtools depth`) into contiguous segments based on position.
+#' This function works with whole genome or specified chromosomes.
+#' The coverage file should contain columns for chromosome, position and depth, with no header.
 #' Attention, currently only supports coverage file in raw text or .gz/.bz2 compressed format.
 #'
-#' @param sample_sheet A `data.frame`. A data frame containing samples `id` and
-#'     the full path to coverage files `cov_file`.
+#' @param sample_sheet A `data.frame`. The data frame should contain two columns: samples `id` and
+#'     the path to coverage files `cov_file`.
 #' @param output_directory A `character`. The path to the output directory.
-#' @param chromosomes A vector of `character`. Chromosomes to keep.
-#'     (same nomenclature as chromosome name in coverage file).
+#' @param chromosomes A `character` vector. The chromosomes to keep (same nomenclature as chromosome
+#'     in coverage file).
 #'     If argument not specified (default is `NULL`), this filter will be turned off.
-#' @param min_depth An `integer`. The minimum depth to keep. Default is `8`.
+#' @param min_depth A `numeric`. The minimum depth to keep. Default is `8`.
 #' @param bin_path A `list(character)`. A list giving the binary path of `awk` and `bgzip` (or `bgzip2`)
 #'     in case of compressed coverage file.
-#' @param nb_cores An `integer`. The number of CPUs to use. Default is `1`.
+#' @param nb_cores A `numeric`. The number of CPUs to use. Default is `1`.
 #'
 #' @return NULL
 #'
@@ -60,21 +58,23 @@ compress_coverage <- function(
     )
   }
 
-  invisible(
-    parallel::mclapply(sample_sheet$id, mc.cores = nb_cores, function(iid) {
+  invisible(parallel::mclapply(
+    X = seq_along(sample_sheet[["id"]]),
+    mc.cores = min(nb_cores, nrow(sample_sheet)),
+    mc_sample_sheet = sample_sheet,
+    FUN = function(i, mc_sample_sheet) {
+      iid <- mc_sample_sheet[["id"]][i]
       log_file <- file.path(output_directory, paste0(iid, ".log"))
       filtered_file <- file.path(output_directory, paste0(iid, ".cov.filtered"))
       filter_script <- file.path(output_directory, paste0("filter-script_", iid))
-      cov_file <- sample_sheet$cov_file[which(sample_sheet$id %in% iid)]
+      cov_file <- mc_sample_sheet[["cov_file"]][i]
 
       cat(paste0("Analyzing sample ", iid, ":"), file = log_file, sep = "\n")
 
       if(! file.exists(cov_file)) {
         cat(
           "The coverage file does not exist, sample skipped!\n",
-          file = log_file,
-          append = TRUE,
-          sep = "\n"
+          file = log_file, append = TRUE, sep = "\n"
         )
         return(invisible())
       }
@@ -84,9 +84,7 @@ compress_coverage <- function(
 
       cat(
         paste0("Filtering position where coverage is lower than ", min_depth, "X..."),
-        file = log_file,
-        append = TRUE,
-        sep = "\n"
+        file = log_file, append = TRUE, sep = "\n"
       )
 
       filtering_time <- system.time({
@@ -121,11 +119,11 @@ compress_coverage <- function(
         append = TRUE,
         sep = "\n"
       )
-    })
-  )
+    }
+  ))
 
   invisible(
-    lapply(sample_sheet$id, function(iid) {
+    lapply(X = sample_sheet[["id"]], FUN = function(iid) {
       log_file <- file.path(output_directory, paste0(iid, ".log"))
       filtered_file <- file.path(output_directory, paste0(iid, ".cov.filtered"))
       compressed_file <- file.path(output_directory, paste0(iid, ".cov.compress"))
@@ -135,7 +133,7 @@ compress_coverage <- function(
 
         if (file.size(filtered_file) == 0) {
           cat(
-            paste0("No position with coverage greater than ", min_depth, "X!"),
+            paste0("No position with coverage greater than ", min_depth),
             file = log_file,
             append = TRUE,
             sep = "\n"
@@ -165,11 +163,12 @@ compress_coverage <- function(
             f = cumsum(c(1, diff(sub_file[[ncol(sub_file) - 1]]) != 1))
           )
 
-          data.table::data.table(
+          res_chr <- data.table::data.table(
             "chr" = jchr,
             "start" = sapply(tmp, `[[`, 1L),
             "end" = sapply(tmp, utils::tail, 1L)
           )
+          return(res_chr)
         }))
 
         invisible(data.table::fwrite(
